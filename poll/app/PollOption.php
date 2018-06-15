bb49a9543891c36f940f91a31290dc88825fd4d1
Enrique Calvo <encalram@gmail.com>
2018-06-15 12:47:26 +0200

    liva delete

--- poll/app/PollOption.php
+++ poll/app/PollOption.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Poll;
 
 class PollOption extends Model
 {
-    //
+    public function polls() 
+    {
+    return $this->belongsToMany(Poll::class, 'poll_options');
+    }
 }
