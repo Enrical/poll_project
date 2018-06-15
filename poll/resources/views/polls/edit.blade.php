8060d810f3cf2142eb5d59aec069cec39704e060
Enrique Calvo <encalram@gmail.com>
2018-06-15 11:34:30 +0200

    views 2

--- poll/resources/views/polls/edit.blade.php
+++ poll/resources/views/polls/edit.blade.php
@@ -2,13 +2,18 @@ Edit view
 <form action="{{ action('PollController@update', ['id' => $poll->id]) }}" method="post">
 {{ csrf_field() }}
 <div class="form-group prl-5">
-{!! Form::label('title', 'Poll title') !!}
-{!! Form::text('title', $poll->name) !!}
+{!! Form::label('name', 'Poll name') !!}
+{!! Form::text('name', $poll->name) !!}
 </div>
 
 <div class="form-group prl-5">
-{!! Form::label('year', 'Year') !!}
-{!! Form::text('year', $poll->year) !!}
+{!! Form::label('description', 'description') !!}
+{!! Form::text('description', $poll->description) !!}
+</div>
+
+<div class="form-group prl-5">
+    {!! Form::label('number of options', 'number of options') !!}
+    {!! Form::text('number of options', $poll->nr_of_options) !!}
 </div>
 
 <div class="form-group prl-5">
--- poll/resources/views/polls/index.blade.php
+++ poll/resources/views/polls/index.blade.php
@@ -1,23 +1,17 @@
-@extends('layouts.app')
+@extends('layout')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    {{-- @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div> --}}
-                    @endif
-
-                    Welcome to the polls!
-                </div>
-            </div>
-        </div>
+<section id="polls">
+    <div class="container">
+        <h1> List </h1>
+        <ul class="list-group">
+            @foreach($polls as $poll)
+                <li class="list-group-item">
+                    {{ $poll->title }}
+                </li>
+            @endforeach
+        </ul>
     </div>
-</div>
+</section>   
 @endsection
